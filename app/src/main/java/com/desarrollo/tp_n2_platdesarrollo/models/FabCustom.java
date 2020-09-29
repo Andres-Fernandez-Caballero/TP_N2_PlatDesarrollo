e842b2b64611e0259ca1aa2b7bb1f9679bf7e19e
lezzzca <lezzzca@gmail.com>
2020-09-28 23:56:11 -0300

    Agregado boton flotante personalizado * Se creo una clase FabCustom que hereda de FloatingActionButton para implemen* Se creo una clase personalizada que hereda de Floati * fab personalizado. * se esta tratando de desarrollar la medicion en segundo plano

--- app/src/main/java/com/desarrollo/tp_n2_platdesarrollo/models/FabCustom.java
+++ app/src/main/java/com/desarrollo/tp_n2_platdesarrollo/models/FabCustom.java
@@ -1,20 +1,50 @@
 package com.desarrollo.tp_n2_platdesarrollo.models;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.util.AttributeSet;
 import android.view.View;
 
+import com.desarrollo.tp_n2_platdesarrollo.ui.home.HomeFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class FabCustom extends FloatingActionButton implements View.OnClickListener {
+public class FabCustom extends FloatingActionButton  {
 
+    private boolean activated;
 
     public FabCustom(Context context) {
         super(context);
+        this.activar();
+    }
+
+    public FabCustom(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        activar();
+    }
+
+    public FabCustom(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        activar();
     }
 
 
     @Override
-    public void onClick(View v) {
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void desactivar(){
+        //TODO: hacer que se desactive el boton y cambie de aspecto
+        this.activated = false;
+        this.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(0, 145, 234)));
+
+    }
+
+    public void activar(){
+        //TODO: hacer que se active y vuelva a su aspecto original
+        this.activated = true;
+        this.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(3, 218, 197))); //colorAccent
 
     }
 }
--- app/src/main/java/com/desarrollo/tp_n2_platdesarrollo/ui/home/HomeFragment.java
+++ app/src/main/java/com/desarrollo/tp_n2_platdesarrollo/ui/home/HomeFragment.java
@@ -1,10 +1,22 @@
 package com.desarrollo.tp_n2_platdesarrollo.ui.home;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +24,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.desarrollo.tp_n2_platdesarrollo.MainActivity;
 import com.desarrollo.tp_n2_platdesarrollo.R;
+import com.desarrollo.tp_n2_platdesarrollo.models.FabCustom;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,6 +34,8 @@ public class HomeFragment extends Fragment {
 
     //private HomeViewModel homeViewModel;
 
+    private ProgressBar progressBar;
+    private  SensorManager manager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,6 +54,9 @@ public class HomeFragment extends Fragment {
             }
         });
         */
+
+
+
         final TextView tMedicion = root.findViewById(R.id.tMedicion);
         /*
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -47,14 +66,138 @@ public class HomeFragment extends Fragment {
             }
         });
          */
-        FloatingActionButton fab = root.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        progressBar = root.findViewById(R.id.progressBar);
+
+        FabCustom fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new OnClickMedicion());
         return root;
     }
+
+    private class OnClickMedicion implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+
+            Timer task = new Timer();
+
+            if(view.getClass() == FabCustom.class){
+
+                FabCustom fab = (FabCustom) view;
+
+                if (fab.isActivated()) { // desactiva el boton y ejecuta la tarea
+                    fab.desactivar();
+                    task.execute();
+
+                } else { // activa el boton y finaliza la tarea
+                    fab.activar();
+                }
+
+
+            }
+        }
+    }
+
+    public  class Timer extends AsyncTask<Void, Integer, Boolean> implements SensorEventListener {
+
+        Long firstTime;
+        Long LastTime;
+        Long tend;
+        Long periodo;
+        Sensor sensor;
+        boolean flagUp;
+        boolean flagDown;
+
+        float medicionAnterior;
+        float medicion;
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+            manager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+            sensor = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+            firstTime = Long.valueOf(-1);
+
+
+            manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_FASTEST);
+
+            periodo = 0L;
+            while(true){
+
+                if(flagUp && !flagDown){
+
+                }
+
+                Log.i("EVENTO", flagUp? "pulso positivo": "pulso bajo");
+            }
+
+
+
+
+          //  return true;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            int progreso = values[0].intValue();
+            progressBar.setProgress(progreso);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setMax(100);
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.VISIBLE);
+
+
+            // TODO: valores de prueba refactorizar despues
+            flagUp = false;
+            flagDown = false;
+
+            medicionAnterior = -1;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+
+            super.onPostExecute(aBoolean);
+            Toast.makeText(getContext(),"Tarea Finalizada en " + tend , Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Toast.makeText(getContext(),"Tarea Cancelada",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float medicion = sensorEvent.values[0];
+            float dmax = sensorEvent.sensor.getMaximumRange();
+
+            if(medicion != dmax) { // flag decendente
+                flagDown =true;
+                flagUp = false;
+                medicionAnterior = medicion;
+
+            }else if(medicionAnterior != dmax && medicion == dmax){ //flag acendente
+                medicionAnterior = medicion;
+                flagUp = true;
+                flagDown = false;
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+            // no se usa ya que la presicion del sensor de proximidad no va a cambiar
+        }
+    }
+
 }
